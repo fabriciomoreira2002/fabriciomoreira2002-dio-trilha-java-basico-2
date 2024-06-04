92353ef0e76f43bb1f4dcb688c9a8d9f7d2ab531
fabriciomoreira2002 <fabriciomoreiracostacst13@gmail.com>
2024-06-03 22:34:45 -0300

    terminado projeto

--- conta-bancaria/src/ContaTerminal.java
+++ conta-bancaria/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+    System.out.print("digite o número da sua conta ");
+        int numero = scanner.nextInt();
+
+    System.out.print("Por favor, Digite o número da sua agência ");
+        String agencia = scanner.next();
+        
+    System.out.print("Por favor, digite o seu nome ");
+        String nomeCliente = scanner.next();
+
+    System.out.print("Por favor, digite o seu saldo ");
+        double saldo = scanner.nextDouble();
+
+    System.out.printf("Olá "+ nomeCliente);
+
+    System.out.print(" Obrigado por criar sua conta em nosso Banco");
+    
+    System.out.print(" Sua agência é "+agencia);
+
+    System.out.print(" Conta "+numero);
+
+    System.out.print(" Seu saldo é " +saldo);
+
+    System.out.print(" já está disponivel para saque");
     }
 }
